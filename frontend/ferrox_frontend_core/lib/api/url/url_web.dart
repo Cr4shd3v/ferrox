4e93a301649bdf3294b06a8aa02216d9a60fef6c
Cr4shd3v <n.hack2810@gmail.com>
2024-10-31 22:15:13 +0100

    fix wrong url in debug

--- frontend/ferrox_frontend_core/lib/api/url/url_web.dart
+++ frontend/ferrox_frontend_core/lib/api/url/url_web.dart
@@ -1,5 +1,11 @@
 import 'dart:html' as html;
 
+import 'package:flutter/foundation.dart';
+
 String getBaseUrl(int devPort) {
-  return '${html.document.baseUri!}/api';
+  if (kDebugMode) {
+    return 'http://127.0.0.1:$devPort/api';
+  } else {
+    return '${html.document.baseUri!}/api';
+  }
 }
